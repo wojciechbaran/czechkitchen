f950d0e7dd851f409c24f127d07ca9d226435448
Wojciech Baran <baran.wojciech@gmail.com>
2017-01-23 14:21:25 +0100

    make menu list

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,5 +4,11 @@ class SiteController < ApplicationController
   def order
   end
   def menu
+  	@starters = Food.where(food_type: 'Starters')
+  	@courses = Food.where(food_type: 'Main Courses')
+  	@soups = Food.where(food_type: 'Soups')
+  	@desserts = Food.where(food_type: 'Desserts')
+  	@beverages = Food.where(food_type: 'Beverages')
+  	@beers = Food.where(food_type: 'Beers')
   end
 end
