b50a15e2d2faf559ecce256f61ae02a309128246
Wojciech Baran <baran.wojciech@gmail.com>
2017-01-26 13:57:54 +0100

    refactoring controllers and testing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,44 +49,32 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
 
-    respond_to do |format|
       if @order.save
-        format.html { redirect_to :action => 'starters', notice: 'Order was successfully created.', order_id: @order.id }
-        format.json { render :show, status: :created, location: @order }
+        redirect_to :action => 'starters', notice: 'Order was successfully created.', order_id: @order.id 
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+         render :new
       end
-    end
   end
 
   def update
-    respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
+        redirect_to @order, notice: 'Order was successfully updated.' 
       else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+        render :edit 
     end
   end
 
   def destroy
     @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+     redirect_to orders_url, notice: 'Order was successfully destroyed.' 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:name, :surname, :street, :number, :city, :phone)
     end
